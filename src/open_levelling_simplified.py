e5fe7fa79e8c9b219603401108a511e4a0ec4266
Batuhan Aktas <aktassbatuhan@gmail.com>
2021-06-19 12:37:14 +0300

    Open Levelling Computation

--- src/open_levelling_simplified.py
+++ src/open_levelling_simplified.py
@@ -22,9 +22,59 @@ def Same_except(error_message):
         pass
     else:
         sys.exit()
-if len(point_value_list) != len(set(point_value_list)):
+if len(point_list) != len(set(point_list)):
     print('\n'+'You Should Not Have Same Point ID' + '\n' + 'That Can Cause Conflict')
     error_message = input('Do You Want To Continue? (Not Recommended) (y/n): ')
     Same_except(error_message)
 else:
     pass   
+
+#  --- BACK AND FORE SIGHT READING SECTION --- 
+bs_read_known = float(input(f'{"Enter the BS reading of point"} {point_list[0]} {"(m)"} {": ":>18}'))  # Back Side Measure of First Point
+# Back Side ID and Value List
+bs_list = [bs_read_known]
+# Fore Side ID and Value List
+fs_list = []
+
+# BACK AND FORE SIGHT VALUE INPUT
+for i in range(number_unknown):
+    if i != (len(point_list) - 2):
+        fs_read = float(input(f'{"Enter the FS reading of point"} {point_list[i + 1]} {"(m)"} {": ":>18}'))
+        fs_list.append(fs_read)
+        bs_read = float(input(f'{"Enter the BS reading of point"} {point_list[i + 1]} {"(m)"} {": ":>18}'))
+        bs_list.append(bs_read)
+    else:
+        fs_read = float(input(f'{"Enter the FS reading of point"} {point_list[i + 1]} {"(m)"} {": ":>18}'))
+        fs_list.append(fs_read)
+
+# --- HEIGHT AND ELEVATION DIFFERENCE SECTION --- 
+# Height Difference ID and Value List
+h_diff_list = []
+# Elevation ID and Value List
+elev_list = [elevation_first]
+
+# --- Calculation Section --- 
+# Height Difference Calculation
+for val in range(len(point_list) - 1):
+    val = bs_list[val] - fs_list[val]
+    h_diff_list.append(val)
+# Elevation Calculation
+for calc in range(len(h_diff_list)):
+    calc = elev_list[calc] + h_diff_list[calc]
+    elev_list.append(calc)
+
+# --- Output Section --- 
+# First Table
+height_calc_title = f'{"Point ID":<12} {"Point ID":<12} {"Delta H"}'
+print('\n' + height_calc_title + '\n' + ('-' * len(height_calc_title)))
+for i in range(len(point_list) - 1):
+    delta_calculation = f'{point_list[i]:<12} {point_list[i + 1]:<12} {format(h_diff_list[i], ".3f")}'
+    print(delta_calculation)
+print(('-' * len(height_calc_title)))
+# Second Table
+elev_calc_title = f'{"Point ID":<12} {"Elevation"}'
+print(elev_calc_title + '\n' + ('-' * len(elev_calc_title)))
+for i in range(len(point_list) - 1):
+    elevation_calculation = f'{point_list[i + 1]:<12} {format(elev_list[i + 1], ".3f")}'
+    print(elevation_calculation)
+print(('-' * len(elev_calc_title)))
